a373907d68732a95b7f77a01c9ecd40fef0f9c77
mdarbyy <michaeldarby2698@gmail.com>
2025-07-17 08:24:06 -0400

    Created Total Sales Dashboard
    created the total sales dashboard

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,4 +1,16 @@
 class StaticPagesController < ApplicationController
   def dashboard
+
+    @start_date = params[:start_date]
+    @end_date = params[:end_date]
+
+    if @start_date.blank? || @end_date.blank?
+      return
+    end
+
+    @sales_records = SalesRecord.where(sell_date: @start_date..@end_date)
+
+    sales_person_ids = @sales_records.pluck(:sales_person_id).uniq
+    @sales_people = SalesPerson.where(id: sales_person_ids)
   end
 end
