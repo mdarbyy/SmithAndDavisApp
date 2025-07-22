61ed50e4a35da3b9c9698d081a8977333f68ec52
mdarbyy <michaeldarby2698@gmail.com>
2025-07-22 09:00:41 -0400

    Added unique record checks for sales person and item
    added unique record checks for sales person and item to make sure duplicate items arent created

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,4 +1,14 @@
 class Item < ApplicationRecord
+  validate :unique_item
+  validates :name, presence: true
+
   has_many :sales_records
-  validates :name, presence: true, uniqueness: true
+
+  private
+
+  def unique_item
+    if Item.where(name: name.titleize).exists?
+      errors.add(:base, "This Item already exists")
+    end
+  end
 end
--- app/models/sales_person.rb
+++ app/models/sales_person.rb
@@ -1,6 +1,15 @@
 class SalesPerson < ApplicationRecord
+  validate :unique_sales_person
   validates :first_name, presence: true
   validates :last_name, presence: true
+  
+  has_many :sales_records
 
-  has_many :sales_records, dependent: :destroy
+  private 
+
+  def unique_sales_person
+    if SalesPerson.where(first_name: first_name.capitalize, last_name: last_name.capitalize).exists?
+      errors.add(:base, "This Sales Person already exists")
+    end
+  end
 end
