e5f4995cc71c6b129a1a6100b249e8ed3139c649
mdarbyy <michaeldarby2698@gmail.com>
2025-09-03 09:06:16 -0400

    Added Amount column to Item Breakdown
    added another column to show the amount of money made from each item
    
    redirects to the previous page if there are no sales records in the specified date range incase the user gets 0 records when they are on the dashboard

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -3,14 +3,14 @@ class StaticPagesController < ApplicationController
     if params[:start_date].blank? || params[:end_date].blank?
       return
     end
-    
+
     @start_date = Date.strptime(params[:start_date], '%m/%d/%Y')
     @end_date = Date.strptime(params[:end_date], '%m/%d/%Y')
 
     @sales_records = SalesRecord.where(sell_date: @start_date..@end_date)
 
     if @sales_records.count == 0
-      redirect_to dashboard_path, danger: "No sales were recorded within this date range"
+      redirect_to request.referrer, danger: "No sales were recorded within this date range"
       return
     end
 
